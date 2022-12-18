a4bdcea5a1f4137ce506fb2a8d3baa8c787ca0e8
Sébastien R <105776390+SRDEVWEB@users.noreply.github.com>
2022-12-18 22:54:00 +0100

    start

---
+++ accueil.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php
+    include_once("templates/menu/header.php");
+    ?>
+    <title>Accueil</title>
+</head>
+
+<body>
+    <header>
+    <?php
+    include_once("templates/menu/menu.php");
+    ?>
+    </header>
+<section class="toppos">
+    <div class="container"><h1 class="text-center">ACCUEIL</h1>
+        <div class="">
+            <h3>Suite de l'accueil...</h3>
+        </div>
+    </div>
+</section>  
+
+    <div class="lapin container bg-info">
+    <?php
+    include_once("templates/menu/accordion.php");
+    ?>
+    </div>
+
+</body>
+</html>
---
+++ templates/menu/menu.php
@@ -0,0 +1,60 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php
+        include_once("templates/menu/header.php");
+    ?>
+<title>Menu</title>
+</head>
+<body>
+
+<nav class="navbartop navbar navbar-expand-lg">
+  <div class="c container-fluid">
+    <a class="a navbar-brand text-center" href="accueil.php"><h1>L'entrepôt<br>du net</h1></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="hamburger navbar-toggler-icon rounded-5"></span>
+    </button>
+    <div class="b collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="lia nav-item">
+          <a class="nav-link disabled">Menu : </a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link" href="#">Mon compte</a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link" href="#">Mes annonces</a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link" href="#">Mon panier</a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link" href="#">Mes recherches sauvegardées</a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link" href="#">Mes achats en cours</a>
+        </li>
+   <!--<li class="lia nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+        <li class="lia nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>-->
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</body>
+</html>
